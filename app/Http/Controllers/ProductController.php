532b5b622d844008bb9de124e9d435be2e564fba
Herbert Fasching <fasching.herbert@outlook.com>
2023-10-29 22:41:32 +0100

    ProductController added

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    function index()
+    {
+        $productName = 'Webserver XL';
+        return view("product", compact("productName"));
+    }
+
+    function show($id)
+    {
+        $productID = $id;
+        return view("showProduct", compact("$productID"));
+    }
+}
