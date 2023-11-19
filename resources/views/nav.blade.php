2dafa388b1e4efdfb08348e0d29d223663a87d0c
Herbert Fasching <fasching.herbert@outlook.com>
2023-11-19 16:09:17 +0100

    added some blade.php for testing purpose

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/info">Index</a><br>
+        </li>
+        <li>
+            <a href="">News</a><br>
+        </li>
+        <li>
+            <a href="">Blog</a><br>
+        </li>
+        <li>
+            <a href="">Contact</a><br>
+        </li>
+    </ul>
+</nav>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,6 +1,15 @@
+@extends('welcome')
+
+@section('title', 'Product')
+
+@section('content')
+
 <h1>Product Page</h1>
 @foreach ($data as $product)
 <h1>{{$product->name}}</h1>
 <p>{{$product->price}}</p>
 <p>{{$product->available}}</p>
 @endforeach
+
+@endsection
+
