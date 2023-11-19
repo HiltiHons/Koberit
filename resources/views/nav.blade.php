eb56ff73d27ef6b866f80bc45bf87d607e0b9ea0
Herbert Fasching <fasching.herbert@outlook.com>
2023-11-19 20:01:24 +0100

    pricing page added

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,16 +1,7 @@
-<nav>
-    <ul>
-        <li>
-            <a href="/info">Index</a><br>
-        </li>
-        <li>
-            <a href="">News</a><br>
-        </li>
-        <li>
-            <a href="">Blog</a><br>
-        </li>
-        <li>
-            <a href="">Contact</a><br>
-        </li>
-    </ul>
-</nav>
+
+<nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="/">Index</a>
+    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">News</a>
+    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Blog</a>
+    <a class="py-2 link-body-emphasis text-decoration-none" href="#">Contact</a>
+  </nav>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,10 +6,25 @@
 
 <h1>Product Page</h1>
 @foreach ($data as $product)
-<h1>{{$product->name}}</h1>
-<p>{{$product->price}}</p>
-<p>{{$product->available}}</p>
+    <h1>{{$product->name}}</h1>
+    <p>{{$product->price}}</p>
+    <p>{{$product->available}}</p>
 @endforeach
+
+@if ($product->price == 7)
+    <p>blade ist geil {{$product->price}}</p> 
+@endif
+
+@unless($product->available == 0)
+und es kann richtig viel {{$product->available}}
+@endunless
+
+<div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">Well done!</h4>
+    <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+    <hr>
+    <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+  </div>
 
 @endsection
 
